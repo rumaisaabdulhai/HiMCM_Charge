477c5768d07a1c64e7d05478e675b7d494cdc672
Rumaisa <32415639+rumaisaabdulhai@users.noreply.github.com>
2019-11-15 21:03:07 -0500

    New Code

--- paytocharge.py
+++ paytocharge.py
@@ -1,13 +1,17 @@
 import random
 import numpy as np
 import pandas as pd
-# need references for computers
 
-num_elements = 1000000
+#############
+# CONSTANTS #
+#############
 
+num_elements = 1
 cost_per_kwh = 0.12
 kwh_per_hour = np.asarray([random.uniform(0.003,0.007) for i in range(num_elements)]) #at least for phones, from 3-7 W: https://www.bijlibachao.com/appliances/charging-a-mobile-phone-how-much-electricity-does-it-consume.html
 cost_per_hour = cost_per_kwh/kwh_per_hour
 price_per_hour = 0.05
 
 annual_net_profit = (price_per_hour-cost_per_hour)*(365.25*24)
+
+print(annual_net_profit)
