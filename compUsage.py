6f3746321deaccd126c7de484ceb74eef707a362
Rumaisa <32415639+rumaisaabdulhai@users.noreply.github.com>
2019-11-15 21:05:21 -0500

    New Code

--- compUsage.py
+++ compUsage.py
@@ -1,22 +1,43 @@
 import random
 import numpy as np
 import pandas as pd
-# need references for computers
 
-num_elements = int(1000000 *0.73) # 73% of adults own laptops as of February 2019 https://www.statista.com/statistics/756054/united-states-adults-desktop-laptop-ownership/
+#############
+# CONSTANTS #
+#############
 
-day_screen_time = 2.21
-# wh_per_hour = random.uniform(50,100)
-wh_per_hour = np.asarray([random.uniform(50,100) for i in range(num_elements)])
+num_elements = int(1000000 * 0.73) # 73% of adults own laptops as of February 2019 https://www.statista.com/statistics/756054/united-states-adults-desktop-laptop-ownership/
 
-wh_per_hour = wh_per_hour*24*365.25
-cost_per_kwh= 0.12
+day_screen_time = 2.21 # hours
+w_per_hour = np.asarray([random.uniform(50,100) for i in range(num_elements)])
 
-cost_per_day = wh_per_hour * (day_screen_time * cost_per_kwh / 1000)
+cost_per_kwh = 0.12
+
+cost_per_day = w_per_hour * (day_screen_time * cost_per_kwh / 1000)
 cost_per_year = cost_per_day * 365.25
 
-def data():
-  df = pd.DataFrame({"WH Per Hour": wh_per_hour,"Cost Per Year": cost_per_year})
+def getData(numel = num_elements):
 
-  pd.DataFrame(df, "CompUsage.csv")
+  numel = int(numel)
+  
+  day_screen_time = 2.21 # hours
+  w_per_hour = np.asarray([random.uniform(50,100) for i in range(num_elements)])
+
+  cost_per_kwh = 0.12
+
+  cost_per_day = w_per_hour * (day_screen_time * cost_per_kwh / 1000)
+  cost_per_year = cost_per_day * 365.25
+
+  data = {"WH Per Hour": w_per_hour,"Cost Per Year": cost_per_year}
+
+  df = pd.DataFrame(data)
+  pd.DataFrame.to_csv(df, "CompUsage.csv")
+  
+  return df
+
+def data():
+  data = {"WH Per Hour": w_per_hour,"Cost Per Year": cost_per_year}
+  df = pd.DataFrame(data)
+  pd.DataFrame.to_csv(df, "CompUsage.csv")
+
   return df
