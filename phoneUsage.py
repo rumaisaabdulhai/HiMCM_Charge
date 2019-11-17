60339eeddbeae15822414cbb370fff8ccdae2f3e
Rumaisa <32415639+rumaisaabdulhai@users.noreply.github.com>
2019-11-17 00:33:53 -0500

    New Changes

--- phoneUsage.py
+++ phoneUsage.py
@@ -2,13 +2,14 @@ import random
 import numpy as np
 
 ''' 
-This python file calculates the number of hours someone spends on their phone on a weekend or weekday. 
+This python file calculates the total time spent in minutes using the phone per day.
 '''
 
 #############
 # CONSTANTS #
 #############
-num_elements = 100 # Specifies the number of elements desired for dataset
+
+num_elements = 1000000 # Specifies the number of elements desired for dataset
 min_percent = 0.70 # [1]
 med_percent = 0.25 # [1]
 max_percent = 0.05 # [1]
@@ -16,81 +17,122 @@ max_percent = 0.05 # [1]
 #############
 # VARIABLES #
 #############
+
 avg_week_clicks = 30 # [1]
 val_range = 10 # chosen range
-hours_spent = np.zeros((num_elements,1))
-hours_spent = hours_spent.fill(10) # number of hours someone spends outside on a weekend
+hours_spent = np.full( (num_elements,1) , 10)
 
-'''
-This method calculates the number of times someone uses their phone on a weekend day.
-'''
-def weekend_picks(hours_spent):
-    x = 58 * np.divide(hours_spent,24)
-    # dividing by 24 to get the day ratio
-    return x
+def get_hours_spent(num_elements, mint = 0, maxt = 10):
+  """
+  Generates a random sample of hours spent outside of home for a person on a day on the weekend.
 
-'''
-This method calculates the number of times
-that a person looks at their phone on a weekday.
-'''
+  :param num_elements: int (number of samples)
+  :param min: int
+  :param max: int
+  :return: numpy array
+  """
 
-def week_picks(num_elements, mode = 0, h = hours_spent):
-  if (mode == 0): # if weekday
-    wp = np.asarray([random.uniform(avg_week_clicks-val_range, avg_week_clicks + val_range) for i in range(num_elements)])
+  hoursspent = np.asarray([random.uniform(minx, maxy) for i in range(num_elements)])
+  return hoursspent
+
+def week_picks(num_elements, mode=True, h=hours_spent):
+  num_elements = int(num_elements)
+  """
+  This method calculates the number of times
+  that a person looks at their phone on a weekday or weekend.
+
+  :param num_elements: int (number of samples)
+  :param mode: int (whether the number of times is calculated for weekdays or weekends (True for weekdays)
+  :param h: numpy array (number of hours spent on phone for each sample)
+  :return: numpy array (number of times phone was used for each sample)
+  """
+  if (mode == True): # if weekday
+    min = avg_week_clicks - val_range
+    max = avg_week_clicks + val_range
+
+    wp = np.asarray([random.uniform(min, max) for i in range(num_elements)])
     return wp
 
   # if time left over, move statement to weekend picks
-  elif (mode == 1): # if weekend
-    picks = weekend_picks(h)
-    wp = np.asarray([random.uniform(weekend_picks(h)[i] - val_range, weekend_picks(h)[i] + val_range) for i in range(num_elements) ])
+  else: # if weekend
+    wp = 58 * np.divide(h, 24)  # dividing by 24 to get the hours to day ratio
     return wp
 
-  else:
-    print("Invalid Day.")
 
 # Calculates required variables for calculating phone usage in a day
 
-'''
-This method calculates a small amount of time a person looks at their phone.  
-'''
 def min_time(num_elements, minrange = 0.5, maxrange = 2):
+  """
+  This method calculates a small amount of time a person looks at their phone.
+  :param num_elements: int (number of samples)
+  :param minrange: int
+  :param maxrange: int
+  :return: numpy array
+  """
   min_time = np.asarray([np.random.uniform(minrange,maxrange) for i in range(num_elements)])
   return min_time
 
-'''
-This method calculates a medium amount of time a person looks at their phone.
-'''
 def med_time(num_elements, minrange = 2, maxrange = 10):
+  """
+  This method calculates a medium amount of time a person looks at their phone.
+  :param num_elements: int (number of samples)
+  :param minrange: int
+  :param maxrange: int
+  :return: numpy array
+  """
   med_time = np.asarray([np.random.uniform(minrange,maxrange) for i in range(num_elements)])
   return med_time
+
 
 '''
 This method calculates the maximum amount of time a person
 '''
-def max_time(num_elements, mu = 17, sigma = 1):
+def max_time(num_elements, mu = 17, sigma = 4):
+  """
+  This method calculates the maximum amount of time a person looks at their phone.
+  An array with the maximum time a person spends looking at their phone per day is generated with random values
+  with mean 17 minutes and a standard deviation of 4 minutes
+  :param num_elements: int
+  :param mu: int
+  :param sigma: int
+  :return: numpy array
+  """
   max_time = np.random.normal(mu, sigma, num_elements)
   return max_time
 
 '''
 This method calculates the phone usage (P) per weekday or weekend day
 '''
-def phoneUsage(avgclicks, valrange, day, num_elements):
-  week_pick = week_picks(num_elements, mode=day)
+def phoneUsage(hoursspent, num_elements = 1, day = True):
+  """
+  This method calculates the total time a person spends using their phone in minutes per day.
+  :param hoursspent:
+  :param num_elements:
+  :param day:
+  :return:
+  """
+
+  week_pick = week_picks(num_elements=num_elements, mode=day, h=hoursspent)
+
   mint = min_time(num_elements)
   medt = med_time(num_elements)
   maxt = max_time(num_elements)
+
+  # seems like the two have different dimensions which makes no sense because num_elements should be the same throughout ikr 
+  
+  print("week_pick ", week_pick)
+  print("mint ", mint)
 
   a = np.multiply(week_pick,mint) * min_percent
   b = np.multiply(week_pick,medt) * med_percent
   c = np.multiply(week_pick,maxt) * max_percent
   
   P = a + b + c
-
   return P
 
 # Main Functions of the Program:
-def weekday(num_elements = 100):
-  return phoneUsage( avgclicks = avg_week_clicks, valrange = val_range, day = 0, num_elements = num_elements)
+def a_weekday(num_elements=100):
+  return phoneUsage(hoursspent=hours_spent, day = True, num_elements = num_elements)
 
-def weekend(hours_spent, num_elements = 100):
-  return phoneUsage(avgclicks = np.average(weekend_picks(hours_spent)), valrange=val_range, day = 1, num_elements = num_elements)
+def a_weekend(hours_spent, num_elements = 100):
+  return phoneUsage(hoursspent = hours_spent, day = False, num_elements = num_elements)
