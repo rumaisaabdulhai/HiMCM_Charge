1cd92952836b6f9a848ebf4f4502558c62345ad9
Rumaisa <32415639+rumaisaabdulhai@users.noreply.github.com>
2019-11-17 19:35:03 -0500

    Updated code

--- otherPlace.py
+++ otherPlace.py
@@ -4,6 +4,17 @@ import random
 import compUsage
 import pandas as pd
 
+#############
+# CONSTANTS #
+#############
+
+num_elements = 1000000
+per_of_adults = 0.55 # [1] Percentage of population that are adults
+adult_phones = 0.81 # [2] Percentage of adult population that owns phones
+weeks_in_year = 365.25/7 # Number of weeks in a year ~= 52
+hours_spent = np.full((int(num_elements),), 10)
+per_of_computers=0.73
+
                     ################## BEG OF CLASS ##################
 class otherPlace:
 
@@ -12,17 +23,12 @@ class otherPlace:
   #############
 
   global weeks_in_year
-  # global mean_low_battery
-  # global battery_deviation
   global mean_low_battery
   
   weeks_in_year = 365.25/7 # Number of weeks in a year ~= 52
   
   # Assumption: You are equally likely to have 0% battery and 100% battery at a place.
   mean_low_battery = 0.20 # Average percentage of people who have a low phone battery
-  
-  # battery_deviation = 0.04 # Deviation of percentage of people who have a low battery
-  # low_batteries = random.uniform(mean_low_battery - battery_deviation, mean_low_battery + battery_deviation) # This is the percentage of foot traffic that needs to charge their phone.
 
   ###############
   # CONSTRUCTOR #
@@ -37,34 +43,42 @@ class otherPlace:
   # ACCESSOR METHODS #
   ####################
   
-  '''
-  This method converts daily to yearly foot traffic and returns it
-  '''
+
   def get_yearly_foot_traffic(self):
+    """
+    This method converts daily to yearly foot traffic and returns it.
+    :return: int
+    """
     return int(weeks_in_year * self.weekly_foot_traffic)
 
-  '''
-  This method returns the minimum time spent at a certain place in hours
-  '''
+
   def get_min_spent_time(self):
+    """
+    This method returns the minimum time spent at a certain place in hours.
+    :return: float
+    """
     return self.avg_time_spent - self.deviation
 
-  '''
-  This method returns the maximum time spent at a certain place in hours
-  '''
+
   def get_max_spent_time(self):
+    """
+    This method returns the maximum time spent at a certain place in hours.
+    :return: float
+    """
     return self.avg_time_spent + self.deviation
   
   '''
-  This method generates various numbers of hours spent on the weekend for each person who visits a certain place for a year.
+  
   '''
   def get_hours_spent(self):
+    """
+    This method generates various numbers of hours spent on the weekend for each person who visits a certain place for a year.
+    :return: numpy array
+    """
     min_time = self.get_min_spent_time()
     max_time = self.get_max_spent_time()
 
-    # low_batteries = random.uniform(mean_low_battery - battery_deviation, mean_low_battery + battery_deviation) # This is the percentage of foot traffic that needs to charge their phone.
-
-    need_to_charge = int(mean_low_battery * self.get_yearly_foot_traffic())
+    need_to_charge = int(mean_low_battery * self.get_yearly_foot_traffic() * adult_phones * per_of_adults)
 
     hours_spent = np.asarray([random.uniform(min_time, max_time) for x in range(need_to_charge)])
     return hours_spent
@@ -73,12 +87,15 @@ class otherPlace:
   # COMPUTE METHODS #
   ###################
 
-  '''
-  This method calculates the yearly cost for all people who need to charge their smartphones at a certain place.
-  '''
-  def PlacesCostSmartphones(self):
 
-    foot_traffic = self.get_yearly_foot_traffic() * mean_low_battery
+  def PlacesCostSmartphones(self):
+    """
+    This method calculates the yearly cost for all people who need to charge their smartphones at a certain place.
+    :return: float, DataFrame
+    """
+
+    foot_traffic = self.get_yearly_foot_traffic() * mean_low_battery * adult_phones * per_of_adults
+    #This is the number of adults with phones that need to recharge their battery in a place.
     hours_spent = self.get_hours_spent()
 
     df = compiling.compileData(int(foot_traffic), hours_spent) # This is the yearly cost of charging smartphones per person.
@@ -89,57 +106,42 @@ class otherPlace:
 
     return yearlyCost, df
 
-  '''
-  This method calculates
-  '''
-  def PlacesCostComputers(self):
 
-    foot_traffic = self.get_yearly_foot_traffic() * mean_low_battery
+  def PlacesCostComputers(self):
+    """
+    This method calculates the yearly cost for all people who need to charge their computers at a certain place.
+    :return: float, DataFrame
+    """
+
+    foot_traffic = self.get_yearly_foot_traffic() * mean_low_battery * per_of_computers * per_of_adults
 
     df = compUsage.getData(foot_traffic)
     yearlyCost = np.sum(df["Total Yearly Cost (per person)"])
 
-    return df
+    return yearlyCost, df
+
                     ################## END OF CLASS ##################
 
 ######################
 # OBJ INITIALIZATION #
 ######################
 
-# mall = otherPlace(1000000/(7 * 365.25/7), 75195, 500)
-# airport = otherPlace(782.85/7, 3.33, 0.33)
-# library = otherPlace(3174.06/7, 2.0, 0.5)
-# coffee_shop = otherPlace(3333.33/7, 0.8, 0.7)
-
-# mall = otherPlace(1000000/(365.25/7), 75195, 500)
-mall = otherPlace(10,5,2)
+mall = otherPlace(1000000/(365.25/7), 75195, 500)
 airport = otherPlace(993.91, 3.33, 0.33)
 library = otherPlace(3174.06, 2.0, 0.5)
 coffee_shop = otherPlace(3333.33, 0.8, 0.7)
 
-# pd.DataFrame.to_csv(mall.PlacesCostComputers(), "mallComputers.csv")
-# pd.DataFrame.to_csv(airport.PlacesCostComputers(), "airportComputers.csv")
-# pd.DataFrame.to_csv(library.PlacesCostComputers(), "libraryComputers.csv")
-pd.DataFrame.to_csv(coffee_shop.PlacesCostComputers(), "coffeeComputers.csv")
-# print(mall.PlacesCostSmartphones())
+def getComputerSmartphoneData():
+  pd.DataFrame.to_csv(mall.PlacesCostComputers()[1], "mallComputers.csv")
+  pd.DataFrame.to_csv(airport.PlacesCostComputers()[1], "airportComputers.csv")
+  pd.DataFrame.to_csv(library.PlacesCostComputers()[1], "libraryComputers.csv")
+  pd.DataFrame.to_csv(coffee_shop.PlacesCostComputers()[1], "coffeeComputers.csv")
 
-'''
-HAVE TO REDO MALLS AND STILL DO COFFEE SHOPS
-'''
+  pd.DataFrame.to_csv(mall.PlacesCostSmartphones()[1], "mallSmartphones.csv")
+  pd.DataFrame.to_csv(airport.PlacesCostSmartphones()[1], "airportSmartphones.csv")
+  pd.DataFrame.to_csv(library.PlacesCostSmartphones()[1], "librarySmartphones.csv")
+  pd.DataFrame.to_csv(coffee_shop.PlacesCostSmartphones()[1], "coffeeSmartphones.csv")
 
-'''
-Airport:
-
-Yearly Passengers in 2018, US: 1,018,339,442 (Page 6)
-https://www.faa.gov/air_traffic/by_the_numbers/media/Air_Traffic_by_the_Numbers_2019.pdf
-https://www.bts.dot.gov/content/number-us-airportsa
-
-Number of Private and Public airports in 2018, US: 19636
-
-1,018,339,442 yearly passengers / 19636 total number of airports = 51860.8393767 passengers per year
-
-51860.84 / (365.25/7) = 993.910679361 passengers per week
-'''
 
 ##############
 # REFERENCES #
@@ -150,7 +152,15 @@ Malls:
   hours spent= 135/60: time found in ICSC survey
 
 Airports:
-  passengers in the us (https://www.worldatlas.com/articles/countries-with-the-highest-number-of-airline-passengers.html), number of us airports (https://www.bts.gov/content/number-us-airportsa)
+  Yearly Passengers in 2018, US: 1,018,339,442 (Page 6)
+  https://www.faa.gov/air_traffic/by_the_numbers/media/Air_Traffic_by_the_Numbers_2019.pdf
+  https://www.bts.dot.gov/content/number-us-airportsa
+
+  Number of Private and Public airports in 2018, US: 19636
+
+  1,018,339,442 yearly passengers / 19636 total number of airports = 51860.8393767 passengers per year
+
+  51860.84 / (365.25/7) = 993.910679361 passengers per week
 
 Libraries:
   num of libraries per year: 
