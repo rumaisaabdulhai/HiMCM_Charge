9764c2b443074338fd710e6fa2cf8374a59a5d97
Rumaisa <32415639+rumaisaabdulhai@users.noreply.github.com>
2019-11-17 19:32:23 -0500

    Updated code

--- compUsage.py
+++ compUsage.py
@@ -6,15 +6,22 @@ import pandas as pd
 # CONSTANTS #
 #############
 
-num_elements = int(1000000 * 0.73) # [1]
+days_in_year = 365.25
+adults_w_laptops = 0.73 # [1]
+num_elements = int(1000000 * adults_w_laptops)
 
-day_screen_time = 2.21 # hours
+day_screen_time = 2.21 # [2]
+
 w_per_hour = np.asarray([random.uniform(50,100) for i in range(num_elements)])
+cost_per_kwh = 0.12 # [3]
 
-cost_per_kwh = 0.12
+kw_per_hour = w_per_hour / 1000 # Metric conversion
+cost_per_day = kw_per_hour * day_screen_time * cost_per_kwh
+cost_per_year = cost_per_day * days_in_year
 
-cost_per_day = w_per_hour * (day_screen_time * cost_per_kwh / 1000)
-cost_per_year = cost_per_day * 365.25
+###################
+# COMPUTE METHODS #
+###################
 
 '''
 This method calculates...
@@ -24,7 +31,7 @@ def getData(numel = num_elements):
   numel = int(numel)
   
   day_screen_time = 2.21 # hours
-  w_per_hour = np.asarray([random.uniform(50,100) for i in range(num_elements)])
+  w_per_hour = np.asarray([random.uniform(50,100) for i in range(numel)])
 
   cost_per_kwh = 0.12
 
@@ -54,5 +61,10 @@ def data():
 
 '''
 [1]: 73% of adults own laptops as of February 2019 https://www.statista.com/statistics/756054/united-states-adults-desktop-laptop-ownership/
+
+[2]:
+
+[3]: Average Cost per KWh
+https://www.npr.org/sections/money/2011/10/27/141766341/the-price-of-electricity-in-your-state
 
 '''
