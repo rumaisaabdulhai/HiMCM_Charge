fc6e82a16becf962d44f44ca096b8b2396691659
Rumaisa <32415639+rumaisaabdulhai@users.noreply.github.com>
2019-11-17 19:11:16 -0500

    Updated code

--- carUsage.py
+++ carUsage.py
@@ -5,40 +5,68 @@ import pandas as pd
 # CONSTANTS #
 #############
 
-chargingStations = 20021 # [1]
+days_in_year = 365.25
+weeks_in_year = 365.25/7 # Number of weeks in a year ~= 52
+
+global num_elements
+
+num_stations = 20021 # [1]
 num_elements = 1000000 # [2]
+cost_per_kwh = 0.12 # [3]
 
-avg_miles_per_year = 13476 # [3]
-avg_miles_per_day = avg_miles_per_year / 365
-mu_1 = avg_miles_per_day
-variance_1 = 5
-miles_per_day = np.random.normal(mu_1, variance_1, num_elements)
+miles_per_year = 13476 # [4]
+miles_per_day = miles_per_year / days_in_year
+mu_1 = miles_per_day
+sigma_1 = 5
 
-cost_per_kwh = 0.12 # [4]
-mu_2 = 0.34 # [5]
-variance_2 = 0.05
-kwh_per_mile = np.random.normal(mu_2, variance_2, num_elements)
+kwh_per_mile = 0.34 # [5]
+mu_2 = kwh_per_mile
+sigma_2 = 0.05
 
-# Energy consumed per week
-kwh_per_week = np.multiply(miles_per_day, kwh_per_mile) * 7
+###################
+# COMPUTE METHODS #
+###################
 
-kwh_per_year = kwh_per_week * (365.25/7)
+'''
+This method generates a random sample of energy consumption per year for a number of num_elements people
+'''
+def get_kwh_per_year():
+  kwh_per_mile = np.random.normal(mu_2, sigma_2, num_elements)
+  miles_per_day = np.random.normal(mu_1, sigma_1, num_elements)
 
-cost_per_year = kwh_per_year * cost_per_kwh
- 
-#Calculates yearly cost for ONE gas station with public charging facilities.
-totalCostperGasStation = np.sum(cost_per_year) / chargingStations
+  miles_per_week = miles_per_day * 7
+  kwh_per_week = np.multiply(miles_per_week, kwh_per_mile) # Energy Consumed per week
 
-def variables():
-  return {"kwh_per_year": np.sum(kwh_per_year), "totalCostperGasStation": totalCostperGasStation}
-  
-def data():
-  df = pd.DataFrame({"KWH per Year": kwh_per_year, "Cost per Year": cost_per_year})
+  kwh_per_year = kwh_per_week * weeks_in_year
+  return kwh_per_year
 
+'''
+This method generates a random sample of cost per year for a number of num_elements people
+'''
+def get_cost_per_year():
+  cost_per_year = get_kwh_per_year() * cost_per_kwh
+  return cost_per_year
+
+'''
+This method calculates the total energy consumption per gas station
+'''
+def get_kwh_per_station():
+  sum_kwh_per_year = np.sum(get_kwh_per_year()) / num_stations
+  return sum_kwh_per_year
+
+'''
+This method calculates the total cost per gas station
+'''
+def get_cost_per_station():
+  totalCostperGasStation = np.sum(get_cost_per_year()) / num_stations
+  return totalCostperGasStation
+
+def getStationData():
+  df = pd.DataFrame({"KWH per Year": get_kwh_per_year(), "Cost per Year": get_cost_per_year()})
   pd.DataFrame.to_csv(df, "CarCostPerPerson.csv")
+
   print("Done!")
   return df
-
 
 ##############
 # REFERENCES #
@@ -50,12 +78,12 @@ def data():
 
  [2]: Estimated number of electric vehicles in US https://www.eei.org/resourcesandmedia/newsroom/Pages/Press%20Releases/EEI%20Celebrates%201%20Million%20Electric%20Vehicles%20on%20U-S-%20Roads.aspx
 
- [3]: Average miles per year
- https://www.autogravity.com/autogravitas/money/whats-average-miles-driven-per-year-car-lease-guide
+ [3]: Average cost per KWh
+ https://www.npr.org/sections/money/2011/10/27/141766341/the-price-of-electricity-in-your-state
 
- [4]: Cost per KWh
+ [4]: Average miles per year
+ https://www.autogravity.com/autogravitas/money/whats-average-miles-driven-per-year-car-lease-guide
 
  [5]: Average KWh per mile
  https://www.fueleconomy.gov/feg/PowerSearch.do?action=PowerSearch&year1=2010&year2=2020&minmsrpsel=0&maxmsrpsel=0&city=0&highway=0&combined=0&cbvtelectric=Electric&YearSel=2010-2020&MakeSel=&MarClassSel=&FuelTypeSel=&VehTypeSel=Electric&TranySel=&DriveTypeSel=&CylindersSel=&MpgSel=000&sortBy=Comb&Units=&url=SearchServlet&opt=new&minmsrp=0&maxmsrp=0&minmpg=&maxmpg=&rowLimit=10
-
 """
